73af095dcd2be3f781a237385d65b694271138c7
Abhash157 <88880166+Abhash157@users.noreply.github.com>
2024-12-12 21:31:53 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+sharebtn = document.getElementsByClassName("share-content")[0]
+sharelink = document.getElementsByClassName("share-link")[0]
+
+sharebtn.addEventListener("click", ()=>{
+    
+})
+// sharelink.addEventListener("mouseover", ()=>{
+//     sharelink.style.backgroundColor = "red"
+// })
+sharelink.addEventListener("click", ()=>{
+    window.location.href = "https://www.facebook.com"
+})
